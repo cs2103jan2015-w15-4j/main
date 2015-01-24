4bfdadceb3cfb94744023238a3a377cdbc73d2ae
jiajunGit <jjgithub@gmail.com>
2015-01-24 15:27:54 +0800

    Added me -Jia Jun

--- source/src/planner/GitPrac.java
+++ source/src/planner/GitPrac.java
@@ -9,11 +9,32 @@ package planner;
  */
 public class GitPrac {
 
+	public static void pauseForASec(){
+		
+		long start = System.currentTimeMillis();
+		while( System.currentTimeMillis() != start + 1000);
+	}
+	
+	public static void countDown(){
+		
+		for( long i = 5; i >= 1; --i ){
+			
+			pauseForASec();
+			System.out.println(i);
+		}
+		
+		System.out.println();
+		pauseForASec();
+	}
+	
 	public static void main(String[] args) {
+		
+		countDown();
+		
 		System.out.println("Wai Kit was here.");
 		System.out.println("Zheng Yi was here too.");
 		System.out.println("Ke Jing was here three");
-		//JiaJun
+		System.out.println("Jia Jun was here :)");
 
 	}
 
