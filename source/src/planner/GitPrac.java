97f3e6876a48fd26c61b7580ae03815a7490d760
Koh Wai Kit <kwkit1992@gmail.com>
2015-01-24 10:22:10 +0800

    Started eclipse project. Added git practice file

---
+++ source/src/planner/GitPrac.java
@@ -0,0 +1,20 @@
+package planner;
+
+/**
+ * 
+ * @author kohwaikit
+ * This class is used for basic git practice. Remove when all is done.
+ * 
+ * Instructions: remove comment on your own name and add a println there.
+ */
+public class GitPrac {
+
+	public static void main(String[] args) {
+		System.out.println("Wai Kit was here.");
+		//ZhengYi
+		//KeJing
+		//JiaJun
+
+	}
+
+}
