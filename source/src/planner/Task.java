633ac8f8a2c6ed8df9faeeccac1dafc889754cda
Koh Wai Kit <kwkit1992@gmail.com>
2015-02-22 22:28:35 +0800

    Added json library. Also defined constructor for Task class.

---
+++ source/src/planner/Task.java
@@ -0,0 +1,14 @@
+package planner;
+
+/**
+ * This class is used to represent the entries for storing tasks.
+ * @author kohwaikit
+ *
+ */
+public class Task {
+    private String taskName, taskDescription;
+    
+    public Task(String name) {
+        
+    }
+}
---
+++ source/test/planner/EntryTest.java
@@ -0,0 +1,15 @@
+package planner;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EntryTest {
+
+    @Test
+    public void CreateTest() {
+        String testName = "entry1";
+        Task entry = new Task(testName);
+    }
+
+}
