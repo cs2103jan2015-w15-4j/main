406ee3866c22d781c7b33deaca8fa56c9b9811d6
Koh Wai Kit <kwkit1992@gmail.com>
2015-02-22 23:04:51 +0800

    Added more fields to Task, added getName method and tests

--- source/src/planner/Task.java
+++ source/src/planner/Task.java
@@ -1,14 +1,34 @@
 package planner;
 
+import java.sql.Timestamp;
+
 /**
  * This class is used to represent the entries for storing tasks.
  * @author kohwaikit
  *
  */
 public class Task {
-    private String taskName, taskDescription;
+    private String taskName, taskDescription, taskTag;
+    private Timestamp dateCreated, dateDue;
+    private int taskPriority;
     
-    public Task(String name) {
+    public Task(String name, String description, Timestamp dueDate, int priority, String tag) throws IllegalArgumentException{
+        if(name.equals("")) {
+            throw new IllegalArgumentException("Task name cannot be empty!");
+        }
         
+        taskName = name;
+        taskDescription = description;
+        taskTag = tag;
+        
+        dateCreated = new Timestamp(System.currentTimeMillis());
+        dateDue = dueDate;
+        
+        taskPriority = priority;
+        
+    }
+    
+    public String getName() {
+        return taskName;
     }
 }
--- source/test/planner/EntryTest.java
+++ source/test/planner/EntryTest.java
@@ -2,14 +2,52 @@ package planner;
 
 import static org.junit.Assert.*;
 
+import java.sql.Timestamp;
+
 import org.junit.Test;
 
 public class EntryTest {
 
     @Test
-    public void CreateTest() {
-        String testName = "entry1";
-        Task entry = new Task(testName);
+    public void createTest() {
+        String taskName = "entry1";
+        String taskDescription = "testing";
+        Timestamp taskDueDate = new Timestamp(System.currentTimeMillis());
+        String taskTag = "nothing";
+        int taskPriority = 5;
+        
+        Task entry = new Task(taskName, taskDescription, taskDueDate, taskPriority, taskTag);
     }
-
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void throwsIllegalArgumentExceptionIfTaskNameIsEmpty() {
+        String emptyTask = "";
+        String taskDescription = "testing";
+        Timestamp taskDueDate = new Timestamp(System.currentTimeMillis());
+        String taskTag = "nothing";
+        int taskPriority = 5;
+        
+        Task empty = new Task(emptyTask, taskDescription, taskDueDate, taskPriority, taskTag);
+    }
+    
+    @Test
+    public void testGetName() {
+        String taskName = "getTask";
+        String taskDescription = "testing";
+        Timestamp taskDueDate = new Timestamp(System.currentTimeMillis());
+        String taskTag = "nothing";
+        int taskPriority = 5;
+        
+        Task entry = new Task(taskName, taskDescription, taskDueDate, taskPriority, taskTag);
+        assertEquals("Task name should be "+taskName, taskName, entry.getName());
+        
+        String taskName2 = "anotherTask";
+        String taskDescription2 = "other";
+        Timestamp taskDueDate2 = new Timestamp(System.currentTimeMillis());
+        String taskTag2 = "others";
+        int taskPriority2 = 3;
+        
+        Task another = new Task(taskName2, taskDescription2, taskDueDate2, taskPriority2, taskTag2);
+        assertEquals("Task name should be "+taskName2, taskName2, another.getName());
+    }
 }
