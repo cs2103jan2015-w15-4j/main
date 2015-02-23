101d3345291687b6fa06413ad4260cc31fdf2261
Koh Wai Kit <kwkit1992@gmail.com>
2015-02-23 17:01:46 +0800

    Added TaskList class

---
+++ source/src/planner/TaskList.java
@@ -0,0 +1,12 @@
+package planner;
+
+import java.util.ArrayList;
+
+/**
+ * TaskList class is a container class for Tasks. It extends ArrayList.
+ * @author kohwaikit
+ *
+ */
+public class TaskList extends ArrayList<Task> {
+    
+}
