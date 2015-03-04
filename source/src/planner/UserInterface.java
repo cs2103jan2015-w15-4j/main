051f18e88b3f06c9731674501ee7329b83b0aadb
Jia Jun <JJGitHub@gmail.com>
2015-03-04 14:01:41 +0800

    Added constructor

--- source/src/planner/UserInterface.java
+++ source/src/planner/UserInterface.java
@@ -1,10 +1,13 @@
 package planner;
 
+import java.awt.LayoutManager;
+
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
 import javax.swing.JScrollPane;
+import org.
 
 /**
  *
@@ -16,14 +19,42 @@ public class UserInterface extends javax.swing.JFrame {
     private JPanel mainFrame;
     private JLabel Background;
     private JLabel closeButton;
-    private JTextField command;
-    private JPanel commandPanel;
-    private JTextPane display;
-    private JScrollPane displayScroll;
     private JLabel dragPanel;
     private JLabel minimiseButton;
+    
+    private JTextField command;
+    
+    private JPanel commandPanel;
+    
+    private JTextPane display;
+    private JScrollPane displayScroll;
+    
     private JTextPane tentative;
     private JScrollPane tentativeScroll;
     
-    
+    public UserInterface(){
+        
+        setVisible(true);
+        
+        minimiseButton = new JLabel();
+        closeButton = new JLabel();
+        dragPanel = new JLabel();
+        Background = new JLabel();
+        
+        commandPanel = new JPanel();
+        command = new JTextField();
+        
+        tentativeScroll = new JScrollPane();
+        tentative = new JTextPane();
+        
+        displayScroll = new JScrollPane();
+        display = new JTextPane();
+        
+        setUndecorated(true);
+        setResizable(false);
+        getContentPane().setLayout(null);
+        
+        pack();
+        setLocationRelativeTo(null);
+    }
 }
