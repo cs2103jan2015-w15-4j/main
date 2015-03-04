6478dea9b079d7c64ab0e97d97a8aac016ef890b
Jia Jun <JJGitHub@gmail.com>
2015-03-04 13:45:49 +0800

    Added essential Jcomponents

--- source/src/planner/UserInterface.java
+++ source/src/planner/UserInterface.java
@@ -1,6 +1,29 @@
 package planner;
 
-public class UserInterface {
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+import javax.swing.JScrollPane;
 
+/**
+ *
+ * @author jia jun
+ */
+public class UserInterface extends javax.swing.JFrame {
+
+    // J-components
+    private JPanel mainFrame;
+    private JLabel Background;
+    private JLabel closeButton;
+    private JTextField command;
+    private JPanel commandPanel;
+    private JTextPane display;
+    private JScrollPane displayScroll;
+    private JLabel dragPanel;
+    private JLabel minimiseButton;
+    private JTextPane tentative;
+    private JScrollPane tentativeScroll;
+    
     
 }
