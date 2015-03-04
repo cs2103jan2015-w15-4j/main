eaf7f783f5a4879e46bea6d00b0c6b27d3106622
Koh Wai Kit <kwkit1992@gmail.com>
2015-03-04 18:40:06 +0800

    Added init and getAllTasks methods

--- source/src/planner/Engine.java
+++ source/src/planner/Engine.java
@@ -1,5 +1,18 @@
 package planner;
 
 public class Engine {
+    private static Configuration config;
+    private static TaskList allTasks;
+    
+    //Not tested yet
+    public static void init() {
+        config = Storage.readConfig();
+        allTasks = Storage.readTaskStorage(config.getStoragePath());
+    }
+    
+    //Not tested yet
+    public static TaskList getAllTasks() {
+        return allTasks;
+    }
     
 }
