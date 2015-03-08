a8295fea1911e42d68fc2535f85ae9730b231675
Koh Wai Kit <kwkit1992@gmail.com>
2015-03-08 16:26:17 +0800

    Added more fields to engine

--- source/src/planner/Engine.java
+++ source/src/planner/Engine.java
@@ -3,11 +3,16 @@ package planner;
 public class Engine {
     private static Configuration config;
     private static TaskList allTasks;
+    private static TaskList doneTasks;
+    private static TaskList undoneTasks;
+    private static TaskList tentativeTasks;
+    private static TaskList normalTasks;
     
     //Not tested yet
     public static void init() {
         config = Storage.readConfig();
         allTasks = Storage.readTaskStorage(config.getStoragePath());
+        
     }
     
     //Not tested yet
