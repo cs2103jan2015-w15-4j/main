672605bc9133a0d4ba4dc7c20e61d5a64e36ea5c
Koh Wai Kit <kwkit1992@gmail.com>
2015-03-08 16:47:48 +0800

    Added basic method stubs for Engine

--- source/src/planner/Engine.java
+++ source/src/planner/Engine.java
@@ -9,10 +9,59 @@ public class Engine {
     private static TaskList normalTasks;
     
     //Not tested yet
-    public static void init() {
-        config = Storage.readConfig();
-        allTasks = Storage.readTaskStorage(config.getStoragePath());
-        
+    public static boolean init() {
+        try {
+            config = Storage.readConfig();
+            allTasks = Storage.readTaskStorage(config.getStoragePath());
+            Logic.splitTaskByDone(allTasks, doneTasks, undoneTasks);
+            Logic.splitTasksByTentative(undoneTasks, normalTasks, tentativeTasks);
+            return true;
+        } catch(Exception e) {
+            return false;
+        }
+    }
+    
+    //Not tested yet
+    public static boolean exit() {
+        try {
+            Storage.saveConfiguration(config);
+            Storage.saveTaskList(config.getStoragePath(), allTasks);
+            return true;
+        } catch(Exception e) {
+            return false;
+        }
+    }
+    
+    //Not tested yet
+    public static Constants.COMMAND_TYPE process(String userInput) {
+        ParseResult result = Parser.parse(userInput);
+        Constants.COMMAND_TYPE command = result.getCommandType();
+        switch (command) {
+            case ADD:
+                //TO BE DONE
+                break;
+            case UPDATE:
+                //TO BE DONE
+                break;
+            case DELETE:
+                //TO BE DONE
+                break;
+            case SHOW:
+                //TO BE DONE
+                break;
+            case DONE:
+                //TO BE DONE
+                break;
+            case UNDO:
+                //TO BE DONE
+                break;
+            case SEARCH:
+                //TO BE DONE
+                break;
+            case HELP:
+                //TO BE DONE
+                break;
+        }
     }
     
     //Not tested yet
@@ -20,4 +69,25 @@ public class Engine {
         return allTasks;
     }
     
+    //Not tested yet
+    public static TaskList getDoneTasks() {
+        return doneTasks;
+    }
+    
+    //Not tested yet
+    public static TaskList getUndoneTasks() {
+        return undoneTasks;
+    }
+    
+    //Not tested yet
+    public static TaskList getTentativeTasks() {
+        return tentativeTasks;
+    }
+    
+    //Not tested yet
+    public static TaskList getConfirmedTasks() {
+        return normalTasks;
+    }
+    
+    //
 }
