2e411d2f817b6bf4c2ae8f4e4e4966b346ec923e
ChengKeJing <purpleplasm@gmail.com>
2015-03-18 13:05:47 +0800

    defended the code

--- source/src/planner/SearchLogic.java
+++ source/src/planner/SearchLogic.java
@@ -5,29 +5,40 @@ import java.util.StringTokenizer;
 public class SearchLogic {
     
     public static void searchAll(TaskList input, TaskList searchList, String wordToLookFor) {
-        for (int i = 0; i < input.size(); i++) {
-            if (containsSearchedWord(input.get(i).getName(), wordToLookFor)) {
-                searchList.add(input.get(i));
+        try {
+            for (int i = 0; i < input.size(); i++) {
+                if (containsSearchedWord(input.get(i).getName(), wordToLookFor)) {
+                    searchList.add(input.get(i));
+                }
+                else if (containsSearchedWord(input.get(i).getDescription(), wordToLookFor)) {
+                    searchList.add(input.get(i));
+                }
+                else if (containsSearchedWord(input.get(i).getTag(), wordToLookFor)) {
+                    searchList.add(input.get(i));
+                }
             }
-            else if (containsSearchedWord(input.get(i).getDescription(), wordToLookFor)) {
-                searchList.add(input.get(i));
-            }
-            else if (containsSearchedWord(input.get(i).getTag(), wordToLookFor)) {
-                searchList.add(input.get(i));
-            }
+        } catch (Exception e) {
+            System.err.println("Invalid input: " + e.getMessage());
         }
     }
     
     public static void searchByTags(TaskList input, TaskList searchList, String tagToLookFor){
-        for (int i = 0; i < input.size(); i++) {
-            if (containsSearchedWord(input.get(i).getTag(), tagToLookFor)) {
-                searchList.add(input.get(i));
+        try {
+            for (int i = 0; i < input.size(); i++) {
+                if (containsSearchedWord(input.get(i).getTag(), tagToLookFor)) {
+                    searchList.add(input.get(i));
+                }
             }
+        } catch (Exception e) {
+            System.err.println("Invalid input: " + e.getMessage());
         }
     }
     
     //Scans for words that matches parts or the whole string, not substring
-    private static boolean containsSearchedWord (String description, String wordToLookFor) {
+    private static boolean containsSearchedWord (String description, String wordToLookFor) throws Exception{
+        if (wordToLookFor.equals("")) {
+            throw new Exception("Cannot search for empty string");
+        }
         StringTokenizer token = new StringTokenizer(description);
         while (token.hasMoreTokens()) {
             String word = token.nextToken();
