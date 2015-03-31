e4acfb19cd8afe3c06bbfb31f6d955cdda7b8c99
Jia Jun <JJGitHub@gmail.com>
2015-03-31 21:03:17 +0800

    Created a class that helps scroll the textbox to show the caret at the end

---
+++ source/src/planner/CustomCaretListener.java
@@ -0,0 +1,56 @@
+package planner;
+
+import java.awt.Rectangle;
+
+import javax.swing.SwingUtilities;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.JTextComponent;
+
+public class CustomCaretListener implements CaretListener{
+
+    private int numOfPixelsToShow;
+    
+    public CustomCaretListener( int numOfPixelsToShow ){
+        
+        setNumOfPixelsToShow(numOfPixelsToShow);
+    }
+    
+    public void setNumOfPixelsToShow( int numOfPixelsToShow ){
+        
+        this.numOfPixelsToShow = numOfPixelsToShow;
+    }
+    
+    public int getNumOfPixelsShown(){
+        
+        return numOfPixelsToShow;
+    }
+    
+    @Override
+    public void caretUpdate( final CaretEvent caretEvent ){
+        
+        SwingUtilities.invokeLater(new Runnable(){
+            
+            @Override
+            public void run(){
+                
+                try{
+                    
+                    JTextComponent textComponent = (JTextComponent)caretEvent.getSource();
+                    
+                    if( textComponent != null ){
+                        
+                        int caretPosition = textComponent.getCaretPosition();
+                        Rectangle visibleRect = textComponent.modelToView(caretPosition);
+                        visibleRect.x = visibleRect.x + numOfPixelsToShow;
+                        textComponent.scrollRectToVisible(visibleRect);
+                    }
+        
+                } catch( BadLocationException badLocationException ){
+                    
+                }
+            }
+        });
+    }
+}
