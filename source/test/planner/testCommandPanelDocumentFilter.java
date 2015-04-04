24287f47a37985e6a9889e4f49190b9069805e58
Jia Jun <JJGitHub@gmail.com>
2015-04-04 17:57:40 +0800

    Added junit tests for methods within CommandPanelDocumentFilter class

---
+++ source/test/planner/testCommandPanelDocumentFilter.java
@@ -0,0 +1,42 @@
+package planner;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testCommandPanelDocumentFilter {
+
+    @Test
+    public void testRegularStringArray() {
+       
+        CommandPanelDocumentFilter commandPanelDocumentFilter = new CommandPanelDocumentFilter();
+        
+        String []keywords = { "nice", "done" };
+        
+        String regexOutput = commandPanelDocumentFilter.generateRegex(keywords);
+        
+        assertEquals( regexOutput, "(\\bnice\\b|\\bdone\\b)" );
+    }
+    
+    @Test
+    public void testStringArrayContainingNull() {
+        
+        CommandPanelDocumentFilter commandPanelDocumentFilter = new CommandPanelDocumentFilter();
+        
+        String []keywords = { "nice", null, "done" };
+        
+        String regexOutput = commandPanelDocumentFilter.generateRegex(keywords);
+        
+        assertEquals( regexOutput, "(\\bnice\\b|\\bdone\\b)" );
+    }
+
+    @Test
+    public void testNullStringArray(){
+        
+        CommandPanelDocumentFilter commandPanelDocumentFilter = new CommandPanelDocumentFilter();
+        
+        String regexOutput = commandPanelDocumentFilter.generateRegex(null);
+        
+        assertEquals( regexOutput, "()" );
+    }
+}
