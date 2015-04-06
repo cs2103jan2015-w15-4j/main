1a8e132738b2cf91d68e7f70326c99e81f60f7a0
Jia Jun <JJGitHub@gmail.com>
2015-04-06 18:22:06 +0800

    Updated reference to renamed CustomNoWrapKit class

---
+++ source/src/planner/NoWrapTextLogic.java
@@ -0,0 +1,37 @@
+package planner;
+
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.BoxView;
+import javax.swing.text.Element;
+import javax.swing.text.LabelView;
+import javax.swing.text.View;
+import javax.swing.text.ViewFactory;
+
+public class NoWrapTextLogic implements ViewFactory{
+
+    @Override
+    public View create( Element element ){
+        
+        if( element == null ){
+            
+            return null;
+        }
+        
+        String property = element.getName();
+        
+        if( property != null ){
+            
+            if( property.equals( AbstractDocument.ParagraphElementName ) ){
+                
+                return new NonWrappingParagraphLayout(element);
+                
+            } else if( property.equals( AbstractDocument.SectionElementName ) ){
+                
+                return new BoxView(element, View.Y_AXIS );
+            }
+            
+        } 
+            
+        return new LabelView(element);
+    }
+}
