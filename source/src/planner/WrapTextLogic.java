314c32360212ae6743e90d86a2553bf570611632
Jia Jun <JJGitHub@gmail.com>
2015-04-06 18:23:33 +0800

    Created new class to handle wrapping text

--- source/src/planner/WrapTextLogic.java
+++ source/src/planner/WrapTextLogic.java
@@ -2,8 +2,12 @@ package planner;
 
 import javax.swing.text.AbstractDocument;
 import javax.swing.text.BoxView;
+import javax.swing.text.ComponentView;
 import javax.swing.text.Element;
+import javax.swing.text.IconView;
 import javax.swing.text.LabelView;
+import javax.swing.text.ParagraphView;
+import javax.swing.text.StyleConstants;
 import javax.swing.text.View;
 import javax.swing.text.ViewFactory;
 
@@ -12,26 +16,32 @@ public class WrapTextLogic implements ViewFactory{
     @Override
     public View create( Element element ){
         
-        if( element == null ){
+        String viewType = element.getName();
+        
+        if( viewType != null ){
             
-            return null;
+            if( viewType.equals( AbstractDocument.ParagraphElementName ) ){
+                
+                return new ParagraphView(element);
+                
+            } else if( viewType.equals( AbstractDocument.ContentElementName ) ){
+                
+                return new WrappingLabelLayout(element);
+                
+            } else if( viewType.equals( AbstractDocument.SectionElementName ) ){
+                
+                return new BoxView( element, View.Y_AXIS );
+                
+            } else if( viewType.equals( StyleConstants.IconElementName ) ){
+                
+                return new IconView(element);
+                
+            } else if( viewType.equals( StyleConstants.ComponentElementName ){
+                
+                return new ComponentView(element);
+            }
         }
         
-        String property = element.getName();
-        
-        if( property != null ){
-            
-            if( property.equals( AbstractDocument.ParagraphElementName ) ){
-                
-                return new NonWrappingParagraphLayout(element);
-                
-            } else if( property.equals( AbstractDocument.SectionElementName ) ){
-                
-                return new BoxView(element, View.Y_AXIS );
-            }
-            
-        } 
-            
         return new LabelView(element);
     }
 }
