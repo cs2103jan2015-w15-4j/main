9a6dd76b8e6ad63db10fde14c0cf6f522d585e53
Jia Jun <JJGitHub@gmail.com>
2015-04-12 06:24:53 +0800

    Added documentation for CustomNoWrapKit class

--- source/src/planner/CustomNoWrapKit.java
+++ source/src/planner/CustomNoWrapKit.java
@@ -5,20 +5,31 @@ package planner;
 import javax.swing.text.StyledEditorKit;
 import javax.swing.text.ViewFactory;
 
+/**
+* The CustomNoWrapKit class is a style editor kit used by text components to prevent text from being wrapped
+* to the next line
+*
+* @author A0111333B
+*/
 public class CustomNoWrapKit extends StyledEditorKit{
 
     private ViewFactory customViewFactory;
     
+    /**
+    * Constructs a style editor kit using a custom view factory
+    */
     public CustomNoWrapKit(){
-        
         super();
-        
         customViewFactory = new NoWrapTextLogic();
     }
     
+    /**
+     * Gets the view factory currently used by this editor kit
+     * 
+     * @return The view factory currently used by this editor kit
+     */
     @Override
     public ViewFactory getViewFactory(){
-        
         return customViewFactory;
     }
 }
