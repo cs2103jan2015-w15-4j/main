1788fd300913ea1f43a21093db45f52e2e721ef6
Jia Jun <JJGitHub@gmail.com>
2015-04-12 08:12:50 +0800

    Updated documentation for CustomWrapKit class

--- source/src/planner/CustomWrapKit.java
+++ source/src/planner/CustomWrapKit.java
@@ -5,20 +5,31 @@ package planner;
 import javax.swing.text.StyledEditorKit;
 import javax.swing.text.ViewFactory;
 
+/**
+* The CustomWrapKit class is a style editor kit used by text components to allow for strings with widths exceeding the 
+* width of the text component to wrap around to the next line
+*
+* @author A0111333B
+*/
 public class CustomWrapKit extends StyledEditorKit{
 
     private ViewFactory defaultViewFactory;
     
+    /**
+     * Constructs a style editor kit using a custom view factory that handles wrap text logic
+     */
     public CustomWrapKit(){
-        
         super();
-        
         defaultViewFactory = new WrapTextLogic();
     }
     
+    /**
+     * Gets the view factory currently used by this editor kit
+     * 
+     * @return The view factory currently used by this editor kit to handle wrap text logic
+     */
     @Override
     public ViewFactory getViewFactory(){
-        
         return defaultViewFactory;
     }
 }
