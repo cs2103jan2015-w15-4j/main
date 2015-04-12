6477cd458d61bb82298f1795b35d03e38a1ffdbd
Jia Jun <JJGitHub@gmail.com>
2015-04-12 08:41:27 +0800

    Added documentation to DisplayStateStack class

--- source/src/planner/DisplayStateStack.java
+++ source/src/planner/DisplayStateStack.java
@@ -4,60 +4,84 @@ package planner;
 
 import java.util.LinkedList;
 
+/**
+* The DisplayStateStack class is a customised stack works like a normal stack but ignores invalid DisplayStates and limits the 
+* number of DisplayStates that exist within the stack by removing some of the oldest DisplayStates that exist on the stack
+*
+* @author A0111333B
+*/
 public class DisplayStateStack {
 
-    private int maxNumOfStates;
-    private LinkedList<DisplayState> stack;
+    private int maxNumOfStates_;
+    private LinkedList<DisplayState> stack_;
     
+    /**
+    * Constructs a DisplayState stack used for storing DisplayStates in chronological order. If maxNumOfDisplayStates is passed
+    * as an integer less than 1, the DisplayStateStack automatically sets the maximum number of DisplayStates that can be stored
+    * within this stack to be 1
+    *
+    * @param maxNumOfDisplayStates  The maximum number of DisplayStates that can be stored within this stack
+    */
     public DisplayStateStack( int maxNumOfDisplayStates ){
-        
-        maxNumOfStates = Math.max( 1, maxNumOfDisplayStates );
-        
-        stack = new LinkedList<DisplayState>();
+        maxNumOfStates_ = Math.max( 1, maxNumOfDisplayStates );
+        stack_ = new LinkedList<DisplayState>();
     }
     
+    /**
+     * Pushes the provided displayState onto the stack. This method will not add any invalid displayStates and null displayStates.
+     *
+     * @param displayState  The displayState to be pushed onto the stack
+     * @return              The displayState provided
+     */
     public DisplayState push( DisplayState displayState ){
-        
         if( displayState != null && (displayState.getKeyEvent() != null || displayState.getCommand() != null) ){
-            
-            while( stack.size() >= maxNumOfStates ){
-                
-                stack.removeLast();
+            while( stack_.size() >= maxNumOfStates_ ){
+                stack_.removeLast();
             }
-            
-            if( stack.size() + 1 <= maxNumOfStates ){
-                
-                stack.push(displayState);
+            if( stack_.size() + 1 <= maxNumOfStates_ ){
+                stack_.push(displayState);
             }
         }
-        
         return displayState;
     }
     
+    /**
+     * Removes and returns the most recent displayState pushed onto the stack.
+     *
+     * @return   The most recent displayState pushed onto the stack.
+     */
     public DisplayState pop(){
-        
-        if( !stack.isEmpty() ){
-            
-            return stack.pop();
-            
+        if( !stack_.isEmpty() ){
+            return stack_.pop();
         } else{
-            
             return null;
         }
     }
     
+    /**
+     * Returns true if the stack currently contains no displayStates, false otherwise
+     *
+     * @return   True if the stack currently contains no displayStates, false otherwise
+     */
     public boolean isEmpty(){
-        
-        return stack.isEmpty();
+        return stack_.isEmpty();
     }
     
+    /**
+     * Returns the most recent displayState pushed onto the stack.
+     *
+     * @return   The most recent displayState pushed onto the stack.
+     */
     public DisplayState peek(){
-        
-        return stack.peek();
+        return stack_.peek();
     }
     
+    /**
+     * Returns the number of displayStates currently present within the stack.
+     *
+     * @return   The number of displayStates currently present within the stack.
+     */
     public int size(){
-        
-        return stack.size();
+        return stack_.size();
     }
 }
